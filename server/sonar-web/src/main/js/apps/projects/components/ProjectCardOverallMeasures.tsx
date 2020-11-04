4cb01401a7023e6991892e06c268416599afb536
ljiLingoport <lji@lingoport.com>
2020-11-03 20:29:26 -0700

    Localyzer

--- server/sonar-web/src/main/js/apps/projects/components/ProjectCardOverallMeasures.tsx
+++ server/sonar-web/src/main/js/apps/projects/components/ProjectCardOverallMeasures.tsx
@@ -145,7 +145,7 @@ export default function ProjectCardOverallMeasures({ measures,lrm }: Props){
             </span>
                 </div>
                 <div className="project-card-measure-label-with-icon">
-                  LRM AVG COMPLETE
+                    Localyzer AVG COMPLETE
                 </div>
               </div>
             </div>
